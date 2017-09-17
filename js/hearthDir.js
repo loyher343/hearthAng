bd8ebaf730b6284981b0c0a0b2e24ff27bec056a
Loy Hernandez <loyhernandez59@gmail.com>
2017-09-17 11:59:53 -0500

    starting directive

--- js/hearthDir.js
+++ js/hearthDir.js
@@ -1,17 +1,17 @@
 angular.module('hearthApp').directive('hearthDir', function(){
     return{
-        restrict: 'EA',         //'E' = element   'A'= atribute
+        restrict: 'E',         //'E' = element   'A'= atribute
         //template: '<div>Hello World</div>'
         templateUrl: '../views/expans.html',
-        scope: {                //isolate scope from ctrl
-            us: '=user',
-            title: '@',
-            greet: '&'          // functions
-        },
-        link: function(scope, element, atributes){
-            console.log(scope)
-            console.log(element)
-            console.log(atributes)
-        }
+        // scope: {                //isolate scope from ctrl
+        //     us: '=user',
+        //     title: '@',
+        //     greet: '&'          // functions
+        // },
+        // link: function(scope, element, atributes){
+        //     console.log(scope)
+        //     console.log(element)
+        //     console.log(atributes)
+        // }
     }
 })
