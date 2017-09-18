c8ce7e6566cfcc01c7e8a0f6e6f140cf26e22c51
Loy Hernandez <loyhernandez59@gmail.com>
2017-09-17 20:29:51 -0500

    changed tmeplateUrl

--- js/hearthDir.js
+++ js/hearthDir.js
@@ -2,7 +2,7 @@ angular.module('hearthApp').directive('hearthDir', function(){
     return{
         restrict: 'E',         //'E' = element   'A'= atribute
         //template: '<div>Hello World</div>'
-        templateUrl: '../views/expans.html',
+        templateUrl: '/views/expans.html',
         // scope: {                //isolate scope from ctrl
         //     us: '=user',
         //     title: '@',
--- js/navDir.js
+++ js/navDir.js
@@ -2,7 +2,7 @@ angular.module('hearthApp').directive('navDir', function(){
     return{
         restrict: 'E',         //'E' = element   'A'= atribute
         //template: '<div>Hello World</div>'
-        templateUrl: '../views/navDir.html',
+        templateUrl: '/views/navDir.html',
         // scope: {                //isolate scope from ctrl
         //     us: '=user',
         //     title: '@',
